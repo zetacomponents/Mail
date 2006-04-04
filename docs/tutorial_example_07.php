16efd5b8a653c4edb81a284663e9c4fe00d6422e
Derick Rethans <github@derickrethans.nl>
2006-04-04 06:53:13 +0000

    - Use pop3. name for Pop3 transport example.

--- docs/tutorial_example_07.php
+++ docs/tutorial_example_07.php
@@ -1,7 +1,7 @@
 <?php
 include( "tutorial_autoload.php" );
 
-$pop3 = new ezcMailPop3Transport( "smtp.example.com" );
+$pop3 = new ezcMailPop3Transport( "pop3.example.com" );
 $pop3->authenticate( "user", "password" );
 $set = $pop3->fetchAll();
 $parser = new ezcMailParser();
