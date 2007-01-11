02fa6cb607e8b52c0ba86bfc5e4aecaccf3741ae
Derick Rethans <github@derickrethans.nl>
2007-01-11 14:53:01 +0000

    - Welcome to 2007.

--- src/parts/multiparts/multipart_digest.php
+++ src/parts/multiparts/multipart_digest.php
@@ -4,7 +4,7 @@
  *
  * @package Mail
  * @version //autogen//
- * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 
--- src/transports/variable/var_set.php
+++ src/transports/variable/var_set.php
@@ -4,7 +4,7 @@ declare(encoding="latin1");
 /**
  * File containing the ezcMailVariableSet class
  *
- * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  * @version //autogentag//
  * @package Mail
