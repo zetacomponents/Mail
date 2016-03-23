e948f8b70873e0c8e130c139a8aca7ef5d25f295
John Corser <john@johncorser.com>
2016-03-23 13:16:38 -0400

    ensure proper spacing between lines

--- src/parser/interfaces/part_parser.php
+++ src/parser/interfaces/part_parser.php
@@ -9,9 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -245,12 +245,12 @@ abstract class ezcMailPartParser
             if ( !in_array( strtolower( $this->lastParsedHeader ), self::$uniqueHeaders ) )
             {
                 $arr = $headers[$this->lastParsedHeader];
-                $arr[0][count( $arr[0] ) - 1] .= str_replace( "\t", " ", $line );
+                $arr[0][count( $arr[0] ) - 1] .= ' ' . ltrim(str_replace( "\t", " ", $line ), ' ');
                 $headers[$this->lastParsedHeader] = $arr;
             }
             else
             {
-                $headers[$this->lastParsedHeader] .= str_replace( "\t", " ", $line );
+                $headers[$this->lastParsedHeader] .= ' ' . ltrim(str_replace( "\t", " ", $line ), ' ');
             }
         }
         // else -invalid syntax, this should never happen.
