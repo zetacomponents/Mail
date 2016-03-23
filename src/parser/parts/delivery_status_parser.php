093d5741948664e5387616d496bdb25f7547765c
John Corser <john@johncorser.com>
2016-03-23 13:20:14 -0400

    if lastParsedHeader is a null (empty) header, ignore the header

--- src/parser/parts/delivery_status_parser.php
+++ src/parser/parts/delivery_status_parser.php
@@ -9,9 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -106,13 +106,14 @@ class ezcMailDeliveryStatusParser extends ezcMailPartParser
             $this->part->createRecipient();
             return;
         }
-        if ( $this->section == 0 )
+        if (isset( $this->lastParsedHeader ) && $this->section == 0 )
         {
             $this->part->message[$this->lastParsedHeader] = $this->headerValue;
         }
         else
         {
-            $this->part->recipients[$this->section - 1][$this->lastParsedHeader] = $this->headerValue;
+            if (isset( $this->lastParsedHeader ))
+                $this->part->recipients[$this->section - 1][$this->lastParsedHeader] = $this->headerValue;
         }
     }
 
