5e6a8eaaa6e13dd2b4e12698a4925af559ed2951
Mike Gillis <mike.gillis@gmomail.org>
2016-08-17 08:50:07 -0500

    Add an explanation as to the second regex in ezcMailImapTransport::fetchFlags()

--- src/transports/imap/imap_transport.php
+++ src/transports/imap/imap_transport.php
@@ -2052,6 +2052,8 @@ class ezcMailImapTransport
                         $parts = explode( ' ', $matches[1] );
                         $flags[intval( $matches[2] )] = $parts;
                     }
+
+                    // The second regex here is to handle edge cases where a mail server like gmail returns the FETCH response in a different order than normal
                     else
                     {
                         preg_match( '/\*\s.*\sFETCH\s\(UID\s(.*)\sFLAGS \((.*)\)\)/U', $response, $matches );
